20d4ed40b7a2c2c6ac22d77cb04db846ed066b96
Snake D Plissken <plisken1.users.noreply.github.com>
2018-05-04 08:06:34 +0100

    initial commit2

--- src/logicbuddy/logicBuddyUI.java
+++ src/logicbuddy/logicBuddyUI.java
@@ -3,6 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+//Ver 1.0.2
+
 package logicbuddy;
 import java.awt.Color;
 import java.awt.datatransfer.*;
